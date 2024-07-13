2008f733b64077549d2f2c2bdff2d6585d27d103
EclipseIsDead <sidmcheath@gmail.com>
2024-07-13 13:14:52 -0400

    draw line prototype

--- asteroids.asm
+++ asteroids.asm
@@ -27,27 +27,16 @@ main:
     # Draw lines on the screen
     lw $a0, test1
     lw $a1, test2
+    lw $a2, dim
+    lw $a3, white
     jal drawLine
 
     # Exit program gracefully
     li $v0, 10
     syscall
 
-# arguments -> $a0: player's position specifically the head of the ship)
-drawPlayer:
-    sw $ra, 0($sp)
-    addi $sp, $sp, -4
-    
-    lw $t0, white       # Load white color
-    add $t2, $s0, $a0   # Calculate actual memory address
-    sw $t0, 0($t1)      # Store white color at calculated address
-    
-    addi $sp, $sp, 4
-    lw $ra, ($sp)
-    jr $ra
-
 # arguments -> $a0: starting point in the frame buffer, $a1: endpoint in the buffer
-# this is an implementation of Brennan's Line algorithm, of which there are 4 cases
+# this is an implementation of Bresenham's Line algorithm, of which there are 4 cases
 # gradient +, delta y < delta x
 # gradient -, delta y < delta x
 # gradient +, delta y > delta x
@@ -58,20 +47,113 @@ drawLine:
     
     lw $t0, dim # grab the bitmap dimension
     div $a0, $t0 # convert to cartesian coords (lo: x, hi: y)
-    mflo $t1
-    mfhi $t2
+    mflo $t1 # x1
+    mfhi $t2 # y1
     div $a1, $t0 # convert to cartesian coords (lo: x, hi: y)
-    mflo $t3
-    mfhi $t4
-    # if delta y < delta x and gradient is positive (i need $t1 and $t2 for later)
-    sub $t0, $t4, $t2
-    sll $t5, $t0, 1 # a = 2(y_1 - y_2) -> $t5 (i don't think this overflows)
-    sub $t0, $t3, $t1
-    sll $t4, $t0, 1 # no longer need $t4
-    sub $t6, $t5, $t4 # b = a - 2(x_1 - x_2) -> $t6
-    sub $t4, $t5, $t0 # p = a - (x_1 - x_2) -> $t4
-    # if p >= 0
+    mflo $t3 # x2
+    mfhi $t4 # y2
+
+    sub $t5, $t3, $t1 # dx = x2 - x1
+    sub $t6, $t4, $t2 # dy = y2 - y1
     
+    abs $t7, $t5 # |dx|
+    abs $t8, $t6 # |dy|
+    
+    # Determine which octant the line is in
+    slt $t9, $t7, $t8 # if |dx| < |dy|, $t9 = 1, else $t9 = 0
+    
+    beqz $t9, x_dominant
+    j y_dominant
+    
+x_dominant:
+    slt $t9, $t5, $zero # if dx < 0, $t9 = 1, else $t9 = 0
+    beqz $t9, x_positive
+    # dx is negative, swap points
+    move $t0, $t1
+    move $t1, $t3
+    move $t3, $t0
+    move $t0, $t2
+    move $t2, $t4
+    move $t4, $t0
+x_positive:
+    sub $t5, $t3, $t1 # recalculate dx
+    sub $t6, $t4, $t2 # recalculate dy
+    sll $t6, $t6, 1 # 2 * dy
+    sub $t7, $t6, $t5 # p = 2 * dy - dx
+    sll $t5, $t5, 1 # 2 * dx
+    j draw_loop_x
+
+y_dominant:
+    slt $t9, $t6, $zero # if dy < 0, $t9 = 1, else $t9 = 0
+    beqz $t9, y_positive
+    # dy is negative, swap points
+    move $t0, $t1
+    move $t1, $t3
+    move $t3, $t0
+    move $t0, $t2
+    move $t2, $t4
+    move $t4, $t0
+y_positive:
+    sub $t5, $t3, $t1 # recalculate dx
+    sub $t6, $t4, $t2 # recalculate dy
+    sll $t5, $t5, 1 # 2 * dx
+    sub $t7, $t5, $t6 # p = 2 * dx - dy
+    sll $t6, $t6, 1 # 2 * dy
+    j draw_loop_y
+
+draw_loop_x:
+    # Draw pixel at (x1, y1)
+    mul $t0, $t2, $a2 # y1 * width
+    add $t0, $t0, $t1 # y1 * width + x1
+    sll $t0, $t0, 2 # (y1 * width + x1) * 4 (4 bytes per pixel)
+    add $t0, $t0, $gp # add base address
+    sw $a3, 0($t0) # store color
+    
+    beq $t1, $t3, exit_draw # if x1 == x2, we're done
+    
+    addi $t1, $t1, 1 # x1++
+    
+    bltz $t7, skip_y_x # if p < 0, skip y increment
+    slt $t9, $t4, $t2 # if y2 < y1, $t9 = 1, else $t9 = 0
+    beqz $t9, inc_y_x
+    addi $t2, $t2, -1 # y1--
+    j update_p_x
+inc_y_x:
+    addi $t2, $t2, 1 # y1++
+update_p_x:
+    sub $t7, $t7, $t5 # p = p - 2dx
+skip_y_x:
+    add $t7, $t7, $t6 # p = p + 2dy
+    
+    j draw_loop_x
+
+draw_loop_y:
+    # Draw pixel at (x1, y1)
+    mul $t0, $t2, $a2 # y1 * width
+    add $t0, $t0, $t1 # y1 * width + x1
+    sll $t0, $t0, 2 # (y1 * width + x1) * 4 (4 bytes per pixel)
+    add $t0, $t0, $gp # add base address
+    sw $a3, 0($t0) # store color
+    
+    beq $t2, $t4, exit_draw # if y1 == y2, we're done
+    
+    addi $t2, $t2, 1 # y1++
+    
+    bltz $t7, skip_x_y # if p < 0, skip x increment
+    slt $t9, $t3, $t1 # if x2 < x1, $t9 = 1, else $t9 = 0
+    beqz $t9, inc_x_y
+    addi $t1, $t1, -1 # x1--
+    j update_p_y
+inc_x_y:
+    addi $t1, $t1, 1 # x1++
+update_p_y:
+    sub $t7, $t7, $t6 # p = p - 2dy
+skip_x_y:
+    add $t7, $t7, $t5 # p = p + 2dx
+    
+    j draw_loop_y
+
+exit_draw:
     addi $sp, $sp, 4
     lw $ra, ($sp)
     jr $ra
