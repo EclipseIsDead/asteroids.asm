28909470d05cdbe04cb148f0b68821e819510e10
EclipseIsDead <sidmcheath@gmail.com>
2024-07-15 14:15:40 -0400

    drawing player attempt

--- asteroids.asm
+++ asteroids.asm
@@ -8,14 +8,17 @@
 #
 # CONVENTIONS:
 # - Every function is expected to have a stack pointer wrapper
-# - Every function is responsible for its own registers and cleanup!
+# - Every function is responsible for its own registers and cleanup
+# - If you run out of argument registers DO NOT move the function body to main,
+#   instead please use temp registers and then cleanup later
 # - 256 * 256 = 0x10000 WHICH IS IMPORTANT
 #####################################################################
 .data
 displayAddress: .word 0x10008000 # hardcoding framebuffer (if you have a different one then change this)
-playerPos: .word 65664  # Example position (128 * 256 + 128)
-test1: .word 65792
-test2: .word 77000
+playerPos: .word 32896  # Example position (128 * 256 + 128)
+theta: .word 1 # rotation of player
+test1: .word 25700 # (100, 100)
+test2: .word 51400 # (200, 200)
 white: .word 0xffffff
 dim: .word 256
 
@@ -24,23 +27,26 @@ dim: .word 256
 lw $s0, displayAddress # $s0 stores the base address for display
 
 main:
-    # Draw lines on the screen
-    lw $a0, test1
-    lw $a1, test2
-    lw $a2, dim
+    # calculate player's 2 child points then draw them with rotation factored in
+    # note player is an isoceles triangle so 10 pixels down, 5 to the right and left
+    lw $a0, playerPos
+    lw $a1, theta
+    lw $a2, dim # should move this to an $sn register rather than arg tbh
     lw $a3, white
-    jal drawLine
+    jal drawPlayer
 
     # Exit program gracefully
     li $v0, 10
     syscall
 
 # arguments -> $a0: starting point in the frame buffer, $a1: endpoint in the buffer
+#           -> $a2: dimension of bitmap, $a3: colour in hex
 # this is an implementation of Bresenham's Line algorithm, of which there are 4 cases
 # gradient +, delta y < delta x
 # gradient -, delta y < delta x
 # gradient +, delta y > delta x
 # gradient -, delta y > delta x
+# this implementation needs to handle overflows gracefully (we are on a torus)
 drawLine:
     sw $ra, 0($sp)
     addi $sp, $sp, -4
@@ -75,6 +81,7 @@ x_dominant:
     move $t0, $t2
     move $t2, $t4
     move $t4, $t0
+
 x_positive:
     sub $t5, $t3, $t1 # recalculate dx
     sub $t6, $t4, $t2 # recalculate dy
@@ -93,6 +100,7 @@ y_dominant:
     move $t0, $t2
     move $t2, $t4
     move $t4, $t0
+
 y_positive:
     sub $t5, $t3, $t1 # recalculate dx
     sub $t6, $t4, $t2 # recalculate dy
@@ -118,6 +126,7 @@ draw_loop_x:
     beqz $t9, inc_y_x
     addi $t2, $t2, -1 # y1--
     j update_p_x
+
 inc_y_x:
     addi $t2, $t2, 1 # y1++
 update_p_x:
@@ -144,6 +153,7 @@ draw_loop_y:
     beqz $t9, inc_x_y
     addi $t1, $t1, -1 # x1--
     j update_p_y
+
 inc_x_y:
     addi $t1, $t1, 1 # x1++
 update_p_y:
@@ -154,6 +164,43 @@ skip_x_y:
     j draw_loop_y
 
 exit_draw:
+    # cleanup registers
+    move $t0, $zero
+    move $t1, $zero
+    move $t2, $zero
+    move $t3, $zero
+    move $t4, $zero
+    move $t5, $zero
+    move $t6, $zero
+    move $t7, $zero
+    # follow stack pointer
+    addi $sp, $sp, 4
+    lw $ra, ($sp)
+    jr $ra
+
+# arguments -> $a0: player "head" point, $a1: current theta rotation of the player
+#           -> $a2: bitmap dim, $a3: colour
+# we store the angle of the player as theta and apply a 2D rotation matrix (floored trig functions)
+# player dim is constant and then approximated -> down ten then +/- 10 pixels
+drawPlayer:
+    sw $ra, 0($sp)
+    addi $sp, $sp, -4
+    
+    # store theta seperately and then re-load it later
+    # remember $tn is local so we use $sn for this stuff!
+    move $s7, $a1
+    
+    # calculate child point positions and draw them
+    addi $a1, $a0, 1290
+    jal drawLine
+
+    addi $a1, $a0, 300 # something's going on here?
+    jal drawLine
+    
+    # reload arguments
+    move $a1, $s7
+    move $s7, $zero
+    
     addi $sp, $sp, 4
     lw $ra, ($sp)
     jr $ra
